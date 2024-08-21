5c3c47a794a3579be0e0367f3b9162e8ae5add6c
Philipp Grulich <philippgrulich@hotmail.de>
2024-08-22 00:07:05 +0200

    add benchmark

--- nautilus/test/execution-tests/TracingBenchmark.cpp
+++ nautilus/test/execution-tests/TracingBenchmark.cpp
@@ -11,20 +11,76 @@
 
 namespace nautilus::engine {
 
-std::uint64_t Fibonacci(std::uint64_t number) {
-	return number < 2 ? 1 : Fibonacci(number - 1) + Fibonacci(number - 2);
-}
-
 val<int8_t> addFunction(val<int8_t> x) {
 	val<int8_t> y = (int8_t) 2;
 	return y + x;
 }
 
-TEST_CASE("Tracing Benchmark") {
-	BENCHMARK("Fibonacci 35") {
-		auto func = details::createFunctionWrapper(addFunction);
-		return tracing::TraceContext::trace(func);
-	};
+val<int8_t> ifThenElse(val<int8_t> x) {
+	if (x > 42) {
+		x = x + 2;
+	} else {
+		x = x + 1;
+	}
+	return x * 2;
 }
+
+val<int8_t> loop(val<int8_t> x) {
+	for (val<int8_t> i = 0; i < 1000; i++) {
+		x = x + 1;
+	}
+	return x;
+}
+
+static auto tests = std::vector<std::tuple<std::string, std::function<void()>>> {
+    {"add", details::createFunctionWrapper(addFunction)},
+    {"ifThenElse", details::createFunctionWrapper(ifThenElse)},
+    {"loop", details::createFunctionWrapper(loop)},
+};
+
+TEST_CASE("Tracing Benchmark") {
+
+	for (auto& test : tests) {
+		auto func = std::get<1>(test);
+		auto name = std::get<0>(test);
+		Catch::Benchmark::Benchmark("trace_" + name).operator=([&func](Catch::Benchmark::Chronometer meter) { meter.measure([&func] { return tracing::TraceContext::trace(func); }); });
+	}
+}
+
+TEST_CASE("SSA Creation Benchmark") {
+
+	for (auto& test : tests) {
+		auto func = std::get<1>(test);
+		auto name = std::get<0>(test);
+
+		Catch::Benchmark::Benchmark("ssa_" + name).operator=([&func](Catch::Benchmark::Chronometer meter) {
+			std::shared_ptr<tracing::ExecutionTrace> trace = tracing::TraceContext::trace(func);
+			meter.measure([&] {
+				auto ssaCreationPhase = tracing::SSACreationPhase();
+				return ssaCreationPhase.apply(trace);
+			});
+		});
+	}
+}
+
+TEST_CASE("IR Creation Benchmark") {
+
+	for (auto& test : tests) {
+		auto func = std::get<1>(test);
+		auto name = std::get<0>(test);
+
+		Catch::Benchmark::Benchmark("ir_" + name).operator=([&func](Catch::Benchmark::Chronometer meter) {
+			std::shared_ptr<tracing::ExecutionTrace> trace = tracing::TraceContext::trace(func);
+			auto ssaCreationPhase = tracing::SSACreationPhase();
+			trace = ssaCreationPhase.apply(trace);
+
+			meter.measure([&] {
+				auto irConversionPhase = tracing::TraceToIRConversionPhase();
+				return irConversionPhase.apply(trace);
+			});
+		});
+	}
+}
+
 
 } // namespace nautilus::engine
