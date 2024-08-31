fd07d2cab4f9878332d99c13d942d4eb38966c92
Philipp Grulich <philippgrulich@hotmail.de>
2024-08-31 10:03:23 +0200

    update

--- nautilus/test/execution-tests/TracingTest.cpp
+++ nautilus/test/execution-tests/TracingTest.cpp
@@ -193,9 +193,9 @@ TEST_CASE("Expression Trace Test") {
 }
 TEST_CASE("Control-flow Trace Test") {
 	auto tests = std::vector<std::tuple<std::string, std::function<void()>>> {{"ifThenCondition", details::createFunctionWrapper(ifThenCondition)},
-	                                                                          //{"multipleVoidReturnsFunction", details::createFunctionWrapper(multipleVoidReturnsFunction)},
+	                                                                          {"multipleVoidReturnsFunction", details::createFunctionWrapper(multipleVoidReturnsFunction)},
 	                                                                          //{"conditionalReturn", details::createFunctionWrapper(conditionalReturn)},
-	                                                                          {"multipleReturns", details::createFunctionWrapper(multipleReturns)},
+	                                                                          //{"multipleReturns", details::createFunctionWrapper(multipleReturns)},
 	                                                                          {"ifThenElseCondition", details::createFunctionWrapper(ifThenElseCondition)},
 	                                                                          {"nestedIfThenElseCondition", details::createFunctionWrapper(nestedIfThenElseCondition)},
 	                                                                          {"nestedIfNoElseCondition", details::createFunctionWrapper(nestedIfNoElseCondition)},
