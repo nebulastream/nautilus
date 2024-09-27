7879f3c0772bf3fc633d1708fb8f37b07b49af28
Philipp Grulich <philippgrulich@hotmail.de>
2024-09-27 03:04:12 +0200

    add compilation time benchmarks (#50)

--- nautilus/test/benchmark/TracingBenchmark.cpp
+++ nautilus/test/benchmark/TracingBenchmark.cpp
@@ -10,6 +10,7 @@
 #include "StaticLoopFunctions.hpp"
 #include "TracingUtil.hpp"
 #include "nautilus/Engine.hpp"
+#include "nautilus/compiler/backends/mlir/MLIRCompilationBackend.hpp"
 #include "nautilus/compiler/ir/IRGraph.hpp"
 #include "nautilus/config.hpp"
 #include "nautilus/tracing/ExecutionTrace.hpp"
@@ -74,6 +75,45 @@ TEST_CASE("IR Creation Benchmark") {
 				return irConversionPhase.apply(trace);
 			});
 		});
+	}
+}
+
+TEST_CASE("Backend Compilation Benchmark") {
+
+	auto registry = compiler::CompilationBackendRegistry();
+
+	std::vector<std::string> backends = {};
+#ifdef ENABLE_MLIR_BACKEND
+	backends.emplace_back("mlir");
+#endif
+#ifdef ENABLE_C_BACKEND
+	backends.emplace_back("cpp");
+#endif
+#ifdef ENABLE_BC_BACKEND
+	backends.emplace_back("bc");
+#endif
+#ifdef ENABLE_ASMJIT_BACKEND
+	backends.emplace_back("asmjit");
+#endif
+	for (auto& backend : backends) {
+		for (auto& test : tests) {
+			auto func = std::get<1>(test);
+			auto name = std::get<0>(test);
+
+			Catch::Benchmark::Benchmark("backend_compilation_" + backend + "_" + name).operator=([&func, &registry](Catch::Benchmark::Chronometer meter) {
+				std::shared_ptr<tracing::ExecutionTrace> trace = tracing::TraceContext::trace(func);
+				auto ssaCreationPhase = tracing::SSACreationPhase();
+				trace = ssaCreationPhase.apply(trace);
+				auto backend = registry.getBackend("mlir");
+				auto irConversionPhase = tracing::TraceToIRConversionPhase();
+				auto ir = irConversionPhase.apply(trace);
+				auto op = engine::Options();
+				// force compilation for the MLIR backend.
+				op.setOption("mlir.eager_compilation", true);
+				auto dh = compiler::DumpHandler(op, "");
+				meter.measure([&] { return backend->compile(ir, dh, op); });
+			});
+		}
 	}
 }
 
