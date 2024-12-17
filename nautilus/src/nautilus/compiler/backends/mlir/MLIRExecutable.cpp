b425d10f077921e8a5cdddd494661bee60531d1a
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2024-12-17 11:19:59 +0100

    prevents concurrent access into llvms jit
    Apparently the internals of the orc jit are not thread-safe:
    https://lists.llvm.org/pipermail/llvm-dev/2017-November/119108.html
    
    The thread sanitizer in nebulastream complaints about a data-race deep within orc. The mailing lists suggests using a lock around access into orc.

--- nautilus/src/nautilus/compiler/backends/mlir/MLIRExecutable.cpp
+++ nautilus/src/nautilus/compiler/backends/mlir/MLIRExecutable.cpp
@@ -6,10 +6,27 @@
 #include <mlir/IR/MLIRContext.h>
 
 namespace nautilus::compiler::mlir {
+static std::mutex llvm_jit_mutex {};
+
 MLIRExecutable::MLIRExecutable(std::unique_ptr<::mlir::ExecutionEngine> engine) : engine(std::move(engine)) {
 }
-
+MLIRExecutable::~MLIRExecutable() {
+	if (engine) {
+		std::scoped_lock lock(llvm_jit_mutex);
+		engine.reset();
+	}
+}
+MLIRExecutable::MLIRExecutable(MLIRExecutable&& other) noexcept
+    : engine(std::move(other.engine)) {
+}
+MLIRExecutable& MLIRExecutable::operator=(MLIRExecutable&& other) noexcept {
+	if (this == &other)
+		return *this;
+	engine = std::move(other.engine);
+	return *this;
+}
 void* MLIRExecutable::getInvocableFunctionPtr(const std::string& member) {
+	std::scoped_lock lock(llvm_jit_mutex);
 	return engine->lookup(member).get();
 }
 bool MLIRExecutable::hasInvocableFunctionPtr() {
