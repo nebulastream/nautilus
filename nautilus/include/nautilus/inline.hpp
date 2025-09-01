54acd28fb8598f6c646a5bd6a01e3afa1279d84c
Benjamin Greb <benjamingreb01@gmail.com>
2025-09-01 14:18:43 +0200

    add inline function registry

---
+++ nautilus/include/nautilus/inline.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "InlineFunctionRegistry.hpp"
+
+//This needs to exist here, in order to inject the otherwise unused registerFunction declaration into the LLVM IR of the target module
+__attribute__((used)) static const auto bitcodeRegistrationFuncPtrDummyPleaseIgnoreThisThanks = &registerBitcodePleaseIgnoreThisThanks;
+
+//This needs to exist here, in order to inject the otherwise unused registerSymbol declaration into the LLVM IR of the target module
+__attribute__((used)) static const auto symbolRegistrationFuncPtrDummyPleaseIgnoreThisThanks = &registerSymbolPleaseIgnoreThisThanks;
+
+// increment version counter whenever the pass is updated to force re-compilation
+#define NAUT_INLINE __attribute__((annotate("naut_inline_v40")))
