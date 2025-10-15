c2ea2c35ee2cf16b7878e3a2800f9e05f4eba112
Benjamin Greb <benjamingreb01@gmail.com>
2025-10-15 10:46:27 +0200

    add inlining support to mlir backend

--- nautilus/src/nautilus/compiler/backends/mlir/JITCompiler.cpp
+++ nautilus/src/nautilus/compiler/backends/mlir/JITCompiler.cpp
@@ -1,6 +1,9 @@
 
 #include "nautilus/compiler/backends/mlir/JITCompiler.hpp"
+#include "fmt/format.h"
+#include "nautilus/compiler/backends/mlir/LLVMInliningUtils.hpp"
 #include "nautilus/compiler/backends/mlir/MLIRLoweringProvider.hpp"
+#include "nautilus/inline.hpp"
 #include <mlir/ExecutionEngine/OptUtils.h>
 #include <mlir/Target/LLVMIR/Dialect/Builtin/BuiltinToLLVMIRTranslation.h>
 #include <mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h>
@@ -8,11 +11,10 @@
 
 namespace nautilus::compiler::mlir {
 
-std::unique_ptr<::mlir::ExecutionEngine>
-JITCompiler::jitCompileModule(::mlir::OwningOpRef<::mlir::ModuleOp>& mlirModule,
-                              const llvm::function_ref<llvm::Error(llvm::Module*)> optPipeline,
-                              const std::vector<std::string>& jitProxyFunctionSymbols,
-                              const std::vector<void*>& jitProxyFunctionTargetAddresses) {
+std::unique_ptr<::mlir::ExecutionEngine> JITCompiler::jitCompileModule(
+    ::mlir::OwningOpRef<::mlir::ModuleOp>& mlirModule, const llvm::function_ref<llvm::Error(llvm::Module*)> optPipeline,
+    const std::vector<std::string>& jitProxyFunctionSymbols, const std::vector<void*>& jitProxyFunctionTargetAddresses,
+    const engine::Options& nautilusOptions) {
 
 	// Register the translation from MLIR to LLVM IR, which must happen before we
 	// can JIT-compile.
@@ -34,6 +36,20 @@ JITCompiler::jitCompileModule(::mlir::OwningOpRef<::mlir::ModuleOp>& mlirModule,
 			auto address = jitProxyFunctionTargetAddresses.at(i);
 			symbolMap[interner(jitProxyFunctionSymbols.at(i))] = {llvm::orc::ExecutorAddr::fromPtr(address),
 			                                                      llvm::JITSymbolFlags::Exported};
+		}
+
+		if (nautilusOptions.getOptionOrDefault("mlir.inline_invoke_calls", false)) {
+			// TODO possible room for some optimization as it always adds *all* symbols from the symbol registry,
+			// regardless of whether they are used
+
+			// iterate over the symbol table of the inline registry and the symbols to the symbol map
+			// this is used to link inlined functions against the host program
+			for (const auto& [key, value] : InlineFunctionRegistry::instance().getSymbolTable()) {
+				// translate the runtime function address to hex representation to match the names used by the inline
+				// functions
+				auto hexStr = ptrToHex(value);
+				symbolMap[interner(hexStr)] = {llvm::orc::ExecutorAddr::fromPtr(value), llvm::JITSymbolFlags::Exported};
+			}
 		}
 		return symbolMap;
 	};
